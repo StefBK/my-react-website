7dec13eff9aaa475192896abfd07cc6a63ffb392
Stef BK <stefsbk@gmail.com>
2022-04-26 10:35:33 +0200

    Initializing Portfolio

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -2,35 +2,67 @@ import { useEffect, useState } from "react";
 
 const Portfolio = () => {
 
-    let [joke,setJoke]=useState('');
+    let [joke, setJoke] = useState('');
 
-    const loadJoke=()=>{
+    const loadJoke = () => {
         fetch("https://api.chucknorris.io/jokes/random")
-        .then(response=>response.json())
-        .then(data=>{
-            setJoke(data.value);
-            console.log(data);
-        })
+            .then(response => response.json())
+            .then(data => {
+                setJoke(data.value);
+                console.log(data);
+            })
     }
 
-    useEffect(()=>loadJoke(),[]);
+    let [gallery, setGallery] = useState([]);
+
+    const loadPics = () => {
+        fetch("https://picsum.photos/v2/list?page=2&limit=9")
+            .then(response => response.json())
+            .then(data => {
+                setGallery(data);
+                console.log(data);
+            })
+    }
+
+    useEffect(() => {
+        loadJoke();
+        loadPics();
+    }, []);
 
     return (
         <section>
             <div className="container-fluid">
                 <div className="row">
                     <div className="col">
-                    <h1>Portfolio</h1>
-                    <button onClick={loadJoke}>Chouk Maurice ?</button>
-                    <p className="bg-dark text-info">{joke}</p>
+                        <h1>Portfolio</h1>
+                        <button onClick={loadJoke}>Chouk Maurice ?</button>
+                        <p className="bg-dark text-info">{joke}</p>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                    </div>
+                </div>
+                <div className="row">
+                    {
+                        gallery.map((pic) => {
+                            return (
+                                <div key={pic.id} className="col-12 col-md-4">
+                                    <div className="card">
+                                        <img src={pic.download_url} className="img-fluid mb-3 m-md-0 card-img-top" alt={pic.author} />
+                                        <div className="card-body">
+                                            <h5 className="card-title">Card title</h5>
+                                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
             </div>
-            <div className="row">
-                <div className="col">
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                </div>
-            </div>
-        </div>
         </section >
     )
 }
